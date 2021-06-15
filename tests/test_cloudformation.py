dcdc1635e1d8ee3df3e873a28fc48dd655866e70
Patrick Greene <patrick.anton.greene@gmail.com>
2021-06-15 10:39:42 -0400

    Add test of common cloudformation create.

--- tests/test_cloudformation.py
+++ tests/test_cloudformation.py
@@ -1,5 +1,29 @@
-from os import path
+import moto
+import boto3
+import pytest
+from os import path, environ
 from cloudformation.cloudformation import CloudFormationStack, operate_on_stack
+
+
+@pytest.fixture(scope='function')
+def aws_credentials():
+    """Mocked AWS Credentials for moto."""
+    environ['AWS_ACCESS_KEY_ID'] = 'testing'
+    environ['AWS_SECRET_ACCESS_KEY'] = 'testing'
+    environ['AWS_SECURITY_TOKEN'] = 'testing'
+    environ['AWS_SESSION_TOKEN'] = 'testing'
+
+
+@pytest.fixture(scope='function')
+def s3(aws_credentials):
+    with moto.mock_s3():
+        yield boto3.client('s3', region_name='us-east-1')
+
+
+@pytest.fixture(scope='function')
+def cf(aws_credentials):
+    with moto.mock_cloudformation():
+        yield boto3.client('cloudformation', region_name='us-east-1')
 
 
 def test_stack_templates():
@@ -8,3 +32,9 @@ def test_stack_templates():
         config_fpath = stack.get_template_path()
         assert path.exists(config_fpath)
         assert path.abspath(config_fpath) == config_fpath
+
+
+def test_create_common_stack(cf):
+    with open('minerva-config.example.yml', 'r') as conf:
+        operate_on_stack(cf, 'create', 'common', conf)
+    assert True
